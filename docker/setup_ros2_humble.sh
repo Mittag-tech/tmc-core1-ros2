21db6fc7384c699e7e17c0a1acfebfbd275c27d1
mittag-tmc <mittag.tech@gmail.com>
2025-03-14 19:44:15 +0900

    bugfix: refactoring code.

--- docker/setup_ros2_humble.sh
+++ docker/setup_ros2_humble.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
 
 source /opt/ros/humble/setup.bash
-rosdep update && rosdep install --from-paths src --ignore-src -y
-colcon build
-source /home/root/ros2_ws/install/setup.bash
-ros2 run micro_ros_setup create_agent_ws.sh
-ros2 run micro_ros_setup build_agent.sh
+colcon build --symlink-install
 source /home/root/ros2_ws/install/setup.bash
 exec "$@"
