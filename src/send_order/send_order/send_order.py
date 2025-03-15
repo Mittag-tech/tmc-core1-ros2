10b97d4caf32e4c3efceaedf30bc1832d89302cc
mittag-tmc <mittag.tech@gmail.com>
2025-03-15 12:51:49 +0900

    feature: send commands and add deadzone.

--- src/send_order/send_order/send_order.py
+++ src/send_order/send_order/send_order.py
@@ -5,9 +5,13 @@ from std_msgs.msg import Float32MultiArray
 import sys
 import serial
 import yaml
+import math
 from pathlib import Path
 
-BASE = str(Path(__file__).parent.parent.parent.parent.parent.parent.parent)
+BASE = str(Path(__file__).parent.parent.parent.parent)
+SHOOT = "B1"
+DIRECTION = "T4"
+ROLLER = "T3"
 
 
 def load_config(config_file=f"{BASE}/src/config.yaml"):
@@ -20,27 +24,64 @@ def calc_ratio(command: int, offset: int, min: int = -128, max: int = 128):
         return (command - offset) / (max - offset)
     elif command < offset:
         return (command - offset) / abs(min - offset)
+    
+def set_deadzone(data, deadzone):
+    if abs(data) <= deadzone:
+        return 0.0
+    else:
+        return data
 
-def calc_cyber(command, mechanum, offset, max_speed):
-    # serial dataの上から4つがjoy stickの右左の順番と思って記載
-    target_x = max_speed * calc_ratio(command=command[0], offset=offset[0])
-    target_y = max_speed * calc_ratio(command=command[1], offset=offset[1])
-    target_w = max_speed * calc_ratio(command=command[2], offset=offset[2])
+def calc_cyber(command, mechanum, offset, max_speed, rotate, joystick, deadzone):
+    joy_max = joystick["max"]
+    joy_min = joystick["min"]
+    reshape_x = abs(joy_max - command[1])
+    target_x = max_speed * calc_ratio(command=reshape_x, offset=offset[1], min=joy_min, max=joy_max)
+    target_y = max_speed * calc_ratio(command=command[0], offset=offset[0], min=joy_min, max=joy_max)
+    target_w = rotate * calc_ratio(command=command[3], offset=offset[2], min=joy_min, max=joy_max)
     tr =   target_x + target_y + (mechanum[0] + mechanum[1])/mechanum[2] * target_w
     tl = - target_x + target_y + (mechanum[0] + mechanum[1])/mechanum[2] * target_w
     bl = - target_x - target_y + (mechanum[0] + mechanum[1])/mechanum[2] * target_w
     br =   target_x - target_y + (mechanum[0] + mechanum[1])/mechanum[2] * target_w
-    return [tr, tl, bl, br]
+    data = [tr, tl, bl, br]
+    cybergear_data = [set_deadzone(x, deadzone=deadzone) for x in data]
+    return cybergear_data
+
+def bool_toggle(command, mask):
+    shift = int(math.log(mask, 2))
+    button = int(bin(int(command) & mask), 2)
+    servo_command = int(bin(button >> shift), 2)
+    if servo_command == 1:
+        return True
+    else:
+        return False
+    
+def create_servo_data(command, servo, mask):
+    direction = bool_toggle(command=command, mask=mask[DIRECTION])
+    roller = bool_toggle(command=command, mask=mask[ROLLER])
+    shoot = bool_toggle(command=command, mask=mask[SHOOT])
+    if not roller:
+        roller_pwm = servo["roller_pwm"]
+    else:
+        roller_pwm = 0
+    if shoot:
+        shoot_angle = servo["angle"]
+    else:
+        shoot_angle = 0
+    if not direction:
+        servo_data = [shoot_angle, roller_pwm, 0, 0]
+    else:
+        servo_data = [0, 0, shoot_angle, roller_pwm]
+    return [float(x) for x in servo_data]
 
 
 class PublisherCore(Node):
-    def __init__(self, topic_name, port = '/dev/ttyAMA10', baudrate=115200, delay=0.2):
+    def __init__(self, topic_name, port = '/dev/ttyAMA10', baudrate=115200, delay=0.01):
         super().__init__('publisher_core')
         self.publisher = self.create_publisher(Float32MultiArray, topic_name, 10)
         self.topic_name = topic_name
         self.get_logger().info(f'Publishing serial input to {topic_name} for M5')
         
-        self.readSer = serial.Serial(port=port, baudrate=baudrate, timeout=3)
+        self.readSer = serial.Serial(port=port, baudrate=baudrate)
         self.timer = self.create_timer(delay, self.publish_serial)
 
         self.cybergear, self.servo = load_config()
@@ -50,17 +91,25 @@ class PublisherCore(Node):
         try:
             line = self.readSer.readline()
             line = line.strip().decode("utf-8")
-            line = [float.fromhex(f"0x{x}") for x in line.split(",")]
+            line = [x for x in line.split(",")][3:]
+            line = [float.fromhex(f"0x{x}") for x in line]
         except Exception as e:
             self.get_logger().error(f'データ読み取りエラー: {e}')
 
-        cybergear_command = line[4:8]
-        servo_command = line[8:]
+        cybergear_command = line[:4]
+        print(cybergear_command)
+        servo_command = line[4]
         cybergear_data = calc_cyber(command=cybergear_command, 
                                     mechanum=self.cybergear["mechanum"], 
                                     offset=self.cybergear["offset"], 
-                                    max_speed=self.cybergear["speed"])
-        msg.data = cybergear_data.append(servo_command)
+                                    max_speed=self.cybergear["speed"],
+                                    rotate=self.cybergear["rotate"],
+                                    joystick=self.cybergear["joystick"],
+                                    deadzone=self.cybergear["deadzone"])
+        servo_data = create_servo_data(command=servo_command,
+                                       servo=self.servo,
+                                       mask=self.servo["mask"])
+        msg.data = cybergear_data + servo_data
         self.publisher.publish(msg)
         self.get_logger().info(f'Sent to M5: {msg}')
 
@@ -81,7 +130,6 @@ def main(args=None):
     except KeyboardInterrupt:
         pass
     finally:
-        node.listener.stop()
         node.destroy_node()
         node.close()
         rclpy.shutdown()
